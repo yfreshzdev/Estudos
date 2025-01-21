17cd432203ab644f2d0dea48c346bf30d226dd29
Felipe <felipemrbarros@gmail.com>
2025-01-21 18:51:27 -0300

    calculadora terminada

--- JavaScript/Calculadora/script.js
+++ JavaScript/Calculadora/script.js
@@ -1,3 +1,22 @@
 function acaoCalc(tipo, valor) {
-    console.log(tipo, valor);
+    if (tipo === 'acao'){
+        //clear
+        if (valor === 'c'){
+            document.getElementById('resultado').value = ''
+        }
+        //botoes funcoes
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+            document.getElementById('resultado').value +=  valor
+        }
+        //valor calculo
+        if (valor === "="){
+           let valor_campo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = valor_campo;
+            
+        }
+        
+    }else if (tipo === 'valor'){
+        //dar o valor
+        document.getElementById('resultado').value +=  valor
+    }
 }
